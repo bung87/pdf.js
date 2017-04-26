4e2a13955ff1b8bc20599999760d949919bc1036
Bung <zh.bung@gmail.com>
2017-04-26 19:50:30 +0800

    Update app.js

--- web/app.js
+++ web/app.js
@@ -53,7 +53,7 @@ function configure(PDFJS) {
   PDFJS.imageResourcesPath = './images/';
   if (typeof PDFJSDev !== 'undefined' &&
       PDFJSDev.test('FIREFOX || MOZCENTRAL || GENERIC || CHROME')) {
-    PDFJS.workerSrc = '../build/pdf.worker.js';
+    PDFJS.workerSrc = "PDFJSWORKERSRC" in window ? window["PDFJSWORKERSRC"]:'../build/pdf.worker.js';
   }
   if (typeof PDFJSDev === 'undefined' || !PDFJSDev.test('PRODUCTION')) {
     PDFJS.cMapUrl = '../external/bcmaps/';
